97a038c2dd54b356ac3284d34eedc512955055bc
delosrogers <delosrogers@gmail.com>
2020-12-21 16:15:23 -0800

    made app run with nginx

--- quick_run.sh
+++ quick_run.sh
@@ -2,4 +2,4 @@
 
 cd src
 docker build -t gene_data_explorer .
-docker run --rm --network gene_data_mysql_network -p 5001:5001 gene_data_explorer
+docker run --rm --network gene_data_mysql_network -p 80:80 gene_data_explorer
--- src/templates/script.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-    type: "POST",
-    url: "http://localhost/genes",
-    data: {genes: 'hello'},
-    success: function(){
-        console.log("success")
-    },
-    dataType: "JSON"
-});
