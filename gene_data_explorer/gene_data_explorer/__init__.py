b41f1dca6aa9f80d73266d0a4e25d7122516c716
delosrogers <delosrogers@gmail.com>
2021-01-04 22:50:16 -0800

    added clustergrammer viz

--- gene_data_explorer/gene_data_explorer/__init__.py
+++ gene_data_explorer/gene_data_explorer/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
+from gene_data_explorer.credentials import *
 
 app = Flask(__name__)
 
-with open ("./gene_data_explorer/credentials.txt","r") as myfile: passwd = myfile.readlines()[0]
+#with open ("./gene_data_explorer/credentials.txt","r") as myfile: passwd = myfile.readlines()[0]
 host = '127.0.0.1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://web_app:{passwd}@{host}:3306/gene_data?auth_plugin=mysql_native_password'.format(passwd = passwd, host=host)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://web_app:{passwd}@{host}:3306/gene_data?auth_plugin=mysql_native_password'.format(passwd = web_app_passwd, host=host)
 db=SQLAlchemy(app)
 db.Model = automap_base(db.Model)
 db.Model.prepare(db.engine, reflect=True)
