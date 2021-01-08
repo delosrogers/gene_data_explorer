bdd244a21a5c69e0b6ed1e6fd61202dca5e71571
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-01-07 16:11:21 -0800

    implemented google login, still need to limit which users are authorized

--- gene_data_explorer/gene_data_explorer/__init__.py
+++ gene_data_explorer/gene_data_explorer/__init__.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 #with open ("./gene_data_explorer/credentials.txt","r") as myfile: passwd = myfile.readlines()[0]
 if os.getenv('RUNNING_IN_DOCKER') == 'TRUE':
     host = 'gene_data_mysql'
+    port = 3306
 else:
     host = '127.0.0.1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://web_app:{passwd}@{host}:3306/gene_data'.format(passwd = web_app_passwd, host=host)
+    port = 6603
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://web_app:{passwd}@{host}:{port}/gene_data'.format(passwd = web_app_passwd, host=host, port = port)
 db=SQLAlchemy(app)
 db.Model = automap_base(db.Model)
 db.Model.prepare(db.engine, reflect=True)
