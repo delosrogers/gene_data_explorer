c8b80ab430b5ad973a8685ad1776327a56192881
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-02-04 13:38:07 -0800

    more deployment changes

--- quick_run.sh
+++ quick_run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd gene_data_explorer
 docker build -t gene_data_explorer .
 docker start gene_data_mysql
 docker run --rm -it --network gene_data_mysql_network -p 5000:5000 -v ~/openssl:/root/openssl gene_data_explorer
