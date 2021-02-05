38380854775db65cc390ad269fdde676523b0f63
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-02-04 21:32:14 -0800

    changed cert file

--- run_flask.sh
+++ run_flask.sh
@@ -2,4 +2,4 @@
 
 export FLASK_ENV=development
 export FLASK_APP=gene_data_explorer
-flask run --host 0.0.0.0 --port 80 --cert /var/ssl/certs --key /var/ssl/private
+flask run --host 0.0.0.0 --port 80 --cert /var/ssl/certs/${KEY_THUMBPRINT} --key /var/ssl/privat/${KEY_THUMBPRINT}
