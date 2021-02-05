e70b2c9121ae6507d843b17b50e9e36f27414c2e
delosrogers <66216682+delosrogers@users.noreply.github.com>
2021-02-05 11:46:55 -0800

    Update run_flask.sh

--- run_flask.sh
+++ run_flask.sh
@@ -2,5 +2,7 @@
 
 export FLASK_ENV=development
 export FLASK_APP=gene_data_explorer
-flask run --host 0.0.0.0 --port 80 --key "/var/ssl/private/${KEY_THUMBPRINT}.p12"
+openssl pkcs12 -in /var/ssl/private/${KEY_THUMBPRINT}.p12 -out /var/ssl/key -nocerts -nodes
+openssl pkcs12 -in /var/ssl/private/${KEY_THUMBPRINT}.p12 -out /var/ssl/cert -clcerts -nokeys
+flask run --host 0.0.0.0 --port 80 --cert /var/ssl/cert --key "/var/ssl/key"
 #--cert "/var/ssl/certs/${KEY_THUMBPRINT}.der" 
